34f5a7d5df23ffdbf00b414aa63e0ea1d28072f7
Neekzen <neekzen.62@gmail.com>
2025-02-26 15:43:38 +0100

    fix: documentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,10 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://ecv.nathanfourny.com',
   output: 'static',
+  vite: {
+    server: {
+      host: true,
+      allowedHosts: ['ecv.nathanfourny.com']
+    }
+  }
 });
